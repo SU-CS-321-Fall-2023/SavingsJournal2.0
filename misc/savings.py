053fe0f254b91fa7d8d1af00f3de2178c46d1533
osterling925 <omsterling76@gmail.com>
2023-09-20 11:18:29 -0400

    update

--- misc/savings.py
+++ misc/savings.py
@@ -1,15 +1,14 @@
 class SavingsAlgorithms:
 
-    currentsavings = None # find way to load/access value
-    totalneeded = None # find way to load/access value
-    recentlysaved = None # temporary value for when $ is made towards a goal
-    addedgoal = None # temporary value for freshly added goal amount
-    # may reset recentlysaved and addedgoal each time they're compiled into the savings?
+    currentsavings = None # how much $ has already been saved towwards all goals collectively
+    totalneeded = None # how much $ is needed for all goals to be completed (includes amount already collected)
+    recentlysaved = None # 'temporary' value for when $ is made towards a goal
+    addedgoal = None # 'temporary' value for freshly added goal amount
 
     # to do:
     # - connect progressbar variable to progressbar on savings page
     # - connect total savings variables to displayed variables on savings page
-    # - create change-detecting algorithm that will launch the total savings function
+    # find way to load/access value for currentsavings and totalneeded
 
     def progressbar():
         percent = currentsavings / totalneeded
