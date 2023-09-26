48555be8cf3a7307f91424d8f118672c9edd72ae
Sisi Small <smallsianah@gmail.com>
2023-09-26 18:41:20 -0400

    styled sign in page

--- supersaverSignIn/app.py
+++ supersaverSignIn/app.py
@@ -14,6 +14,22 @@ def signIn():
 def signUp():
     return render_template('signUp.html')
 
+@app.route('/spending_habits/')
+def spending_habits():
+    return render_template('spending_habits.html')
+
+@app.route('/total_savings/')
+def total_savings():
+    return render_template('total_savings.html')
+
+@app.route('/savings_journal/')
+def savings_journal():
+    return render_template('savings_journal.html')
+
+@app.route('/index2/')
+def index2():
+    return render_template('index2.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
