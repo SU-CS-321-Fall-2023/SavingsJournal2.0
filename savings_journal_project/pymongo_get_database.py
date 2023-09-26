c1af80edfdc30c6519314367099f8af09543b1a5
Skyler Gipson <smgipson84@gmail.com>
2023-09-26 17:26:58 -0400

    Update pymongo_get_database.py

--- savings_journal_project/pymongo_get_database.py
+++ savings_journal_project/pymongo_get_database.py
@@ -1,3 +1,4 @@
+"""
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
@@ -16,3 +17,4 @@ def get_database():
 
     # Create the database
     return client['user']
+"""
