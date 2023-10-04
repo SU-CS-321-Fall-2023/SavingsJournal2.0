0619c821f0d7c272a52fb6d53ea4a3dd3e86f4b4
Sisi Small <smallsianah@gmail.com>
2023-10-04 11:13:49 -0400

    Added logout

--- supersaverSignIn/app.py
+++ supersaverSignIn/app.py
@@ -1,4 +1,4 @@
-"""
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -58,7 +58,7 @@ def index2():
 
 if __name__ == '__main__':
     app.run(debug=True)
-"""
+
 # SKYLER CODE
 
 from pymongo.server_api import ServerApi
