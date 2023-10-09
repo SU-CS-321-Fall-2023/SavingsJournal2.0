9dc39b39fbd5751f4f50c0de504b06db1a5e56f7
Sisi Small <smallsianah@gmail.com>
2023-10-09 11:31:52 -0400

    Revert "made neater"
    This reverts commit 6d5148c14c9c3bf151a5c4c02d42707bab20002a.

--- supersaverSignIn/app.py
+++ supersaverSignIn/app.py
@@ -1,63 +1,63 @@
 
-from flask import Flask, render_template
+# from flask import Flask, render_template
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route('/') # login/ signup buttons on top
-def index():
-    return render_template('index.html')
+# @app.route('/') # login/ signup buttons on top
+# def index():
+#     return render_template('index.html')
 
-@app.route('/signIn/')
-def signIn():
-    return render_template('signIn.html')
+# @app.route('/signIn/')
+# def signIn():
+#     return render_template('signIn.html')
 
-@app.route('/signUp/')
-def signUp():
-    return render_template('signUp.html')
+# @app.route('/signUp/')
+# def signUp():
+#     return render_template('signUp.html')
 
-@app.route('/spending_habits/')
-def spending_habits():
-    return render_template('spending_habits.html')
+# @app.route('/spending_habits/')
+# def spending_habits():
+#     return render_template('spending_habits.html')
 
-@app.route('/total_savings/')
-def total_savings():
-    return render_template('total_savings.html')
+# @app.route('/total_savings/')
+# def total_savings():
+#     return render_template('total_savings.html')
 
-goals = [
-  {
-    'name': 'Vacation', 
-    'description': 'Trip to Hawaii',
-    'amount': 5000,
-    'deadline': '2024-06-30',
-    'status': 'todo'
-  },
-  {
-    'name': 'Car',
-    'description': 'Downpayment on new car',
-    'amount': 15000, 
-    'deadline': '2025-05-01',
-    'status': 'done'
-  },
-  {
-    'name': 'Roof',
-    'description': 'Fix roof',
-    'amount': 8000,
-    'deadline': '2023-11-15', 
-    'status': 'doing'
-  }
-]
+# goals = [
+#   {
+#     'name': 'Vacation', 
+#     'description': 'Trip to Hawaii',
+#     'amount': 5000,
+#     'deadline': '2024-06-30',
+#     'status': 'todo'
+#   },
+#   {
+#     'name': 'Car',
+#     'description': 'Downpayment on new car',
+#     'amount': 15000, 
+#     'deadline': '2025-05-01',
+#     'status': 'done'
+#   },
+#   {
+#     'name': 'Roof',
+#     'description': 'Fix roof',
+#     'amount': 8000,
+#     'deadline': '2023-11-15', 
+#     'status': 'doing'
+#   }
+# ]
 
-@app.route('/savings_journal/')
-def savings_journal():
-    return render_template('savings_journal.html', goals=goals)
+# @app.route('/savings_journal/')
+# def savings_journal():
+#     return render_template('savings_journal.html', goals=goals)
 
-@app.route('/index2/')
-def index2():
-    return render_template('index2.html')
+# @app.route('/index2/')
+# def index2():
+#     return render_template('index2.html')
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
 
 # SKYLER CODE
 
